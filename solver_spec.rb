86c517f11d3ed68d908c92866610e849486db34a
Abu Raihan <raihan2bd.official@gmail.com>
2023-05-25 18:42:51 +0600

    Create a class called Solver and add a test for it.

---
+++ solver_spec.rb
@@ -0,0 +1,13 @@
+require_relative './solver'
+
+describe Solver do
+  before :each do
+    @solver = Solver.new
+  end
+
+  describe '#new' do
+    it 'returns a Solver object' do
+      expect(@solver).to be_an_instance_of Solver
+    end
+  end
+end
