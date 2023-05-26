8c056d5a116e1731ad5680c04d7ca1fa895facca
isaacg <iwanyoike65@gmail.com>
2023-05-26 11:15:15 +0300

    Create fizzbuzz method and check tests

--- solver.rb
+++ solver.rb
@@ -13,4 +13,16 @@ class Solver
   def reverse(word)
     word.reverse
   end
+
+  def fizzbuzz(num)
+    if (num % 15).zero?
+      'fizzbuzz'
+    elsif (num % 3).zero?
+      'fizz'
+    elsif (num % 5).zero?
+      'buzz'
+    else
+      num.to_s
+    end
+  end
 end
